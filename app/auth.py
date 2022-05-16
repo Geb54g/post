1071a92b272b622f50322056929fcac230def8ca
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-17 00:02:06 +0300

    ft:imported request from flask

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,23 @@
-from flask import Blueprint,render_template,redirect,url_for
+from flask import Blueprint,render_template,redirect,url_for,request
 
 auth = Blueprint("auth",__name__)
 
 
 @auth.route("/login")
 def login():
-    return render_template("login.html")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    return render_template("login.html",methods=['GET','POST'])
 
 
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up",methods=['GET','POST'])
 def sign_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
     return  render_template("signup.html")
 
 
