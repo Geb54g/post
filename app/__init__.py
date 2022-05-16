df50b95d7fe409be7ed994322d070986276047be
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-17 00:17:11 +0300

    ft:created model file for defining database models

--- app/__init__.py
+++ app/__init__.py
@@ -3,23 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy
+DB_NAME ="dabase.db"
+
 
 def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = "U74767YUGUEGECREWVCXVHD"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite://{DB_NAME}'
+    db.init_app(app)
     
     
     from.views import views
     from.auth import auth
     
+
     
     app.register_blueprint(views,url_prefix="/")
     app.register_blueprint(auth,url_prefix="/")
     
+    create_database(app)
     
     
     return app
+
+def create_database(app):
+    if not path.exists("app/" + DB_NAME):
+        db.create_all(app=app)
+        print("created database!")
 
 
 
