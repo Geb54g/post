f80fc25949921ea4ea5f5130611190cb958d9ede
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-16 21:07:55 +0300

    ft:created a sign up and logout routes

--- app/auth.py
+++ app/auth.py
@@ -6,3 +6,16 @@ auth = Blueprint("auth",__name__)
 @auth.route("/login")
 def login():
     return "Login"
+
+
+
+
+@auth.route("/Sign-up")
+def login():
+    return "Login"
+
+
+
+@auth.route("/logout")
+def login():
+    return "Login"
